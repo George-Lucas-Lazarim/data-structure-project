993aeaed9982b6d92bcf5b7faa8bb37e9bd34d5f
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-06-22 16:02:41 -0300

    função init para fila estática

--- queue.c
+++ queue.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "queue.h"
+
+void initStaticCircularQueue (StaticCircularQueue *q) {
+    if (q == NULL) {
+        printf("\nErro! O ponteiro da fila esta apontando para NULL");
+        return;
+    }
+
+    q->start = 0;
+    q->end = -1;
+    q->total_elements = 0;
+}
