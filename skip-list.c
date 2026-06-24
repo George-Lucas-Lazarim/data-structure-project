1f5b1ddd3acbb394855e2a357308f7e4afa1e071
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-06-24 15:17:41 -0300

    função random level

--- skip-list.c
+++ skip-list.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "skip-list.h"
+
+static uint8_t randomLevel (void) {
+    uint8_t level = 1;
+
+    while (level < SKIPLIST_MAX_LEVEL && rand() < SKIPLIST_P_THRESHOLD) level++;
+    
+    return level;
+}
