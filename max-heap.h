90c5670a2b139fefc84581f2c8e2f355c4c9d20b
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-06-23 23:34:40 -0300

    definição das funções

---
+++ max-heap.h
@@ -0,0 +1,19 @@
+#ifndef MAX_HEAP_H
+#define MAX_HEAP_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "hash.h" // struct DTCAlert
+
+typedef struct {
+    DTCAlert* data;
+    uint16_t capacity;
+    uint16_t size;
+} MaxHeap;
+
+void initMaxHeap (MaxHeap* mh, uint16_t capacity);
+bool insertMaxHeap (MaxHeap* mh, DTCAlert* alert);
+DTCAlert extractMax (MaxHeap* pq);
+void freeMaxHeap (MaxHeap* pq);
+
+#endif
